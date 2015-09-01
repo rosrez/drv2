6b3a1788b6791920368a78f213799f3f277a58db
rosrez <rosrez@o2.pl>
2015-09-01 23:45:50 +0200

    massive update of blkstat: all statistics but fifo

--- 55-block/blkstat.c
+++ 55-block/blkstat.c
@@ -10,22 +10,29 @@
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/hdreg.h>
+#include <linux/proc_fs.h>
 
-#define DEVNAME_0 "blkstat0"
+#include <asm/atomic.h>
+
 #define DEVNAME "blkstat"
+#define PROC_ENTRY "blkstat"
 
 #define NR_MINORS 1
 
 #define STACKBD_BDEV_MODE (FMODE_READ | FMODE_WRITE | FMODE_EXCL)
 #define KERNEL_SECTOR_SIZE 512  /* FIXME: */
 
+#define MIN_SAMPLES 100
+
+/* FIXME: set the default to 100000 */
+#define DEF_SAMPLES 100
+
 static int majornr = 0;
 
 static int LOGICAL_BLOCK_SIZE = 512;
 module_param(LOGICAL_BLOCK_SIZE, int, S_IRUGO | S_IWUSR);
 
-/* FIXME: set the default to 100000 */
-static int nrsamples = 100;
+static int nrsamples = DEF_SAMPLES;
 module_param(nrsamples, int, S_IRUGO | S_IWUSR);
 
 char targetname[256];
@@ -38,15 +45,11 @@ struct biostat {
 };
 
 struct binfo {
-    int ios[2];
-    int totrt[2];
+    unsigned long ios[2];
+    unsigned long duration[2];
     /* no explicit mean value - can be derived from the above */
-    int minrt;
-    int maxrt;
-/* FIXME: remove comment */
-#if 0
-    struct ififo *rt;   /* a FIFO with response times (the capacity is nrsamples) */
-#endif
+    unsigned long minrt;
+    unsigned long maxrt;
 };
 
 /* our 'device' structure, contains... everyting */
@@ -59,19 +62,38 @@ struct blkstat {
     struct request_queue *queue;
     sector_t capacity;
 
-    /* the statistics structure */
-    struct binfo binfo;
+    /* the statistics: info + qdepth + rtimes -- protected by infolock */
+    struct binfo info;
+    atomic_t qdepth;
+    /* a FIFO with response times (the capacity is nrsamples) */
+    /* FIXME: remove comment */
+#if 0
+    struct ififo *rt;   
+#endif
 
-    /* protects the binfo structure */
-    spinlock_t slock;
-
-    /* protects the blkstat structure */
-    mutex_t mlock;
+    /* protects the info struct */
+    spinlock_t infolock;
+    /* protects the blkstat struct */
+    struct mutex devlock;
+    /* prevents concurrent access to procfs entry */
+    struct mutex proclock;
 
     int is_active;  /* FIXME: remove this */
 }; 
 
-static struct blkstat blkstat;
+/* a snapshot of statistics from blkstat -- this is presented to user via procfs */
+struct userinfo {
+    struct binfo info;
+    int qdepth;
+    int *rtimes;
+    int rtcnt;
+};
+
+static struct blkstat blkstat = {
+    .qdepth = ATOMIC_INIT(0)
+};
+
+static struct userinfo userinfo;
 
 static struct biostat *alloc_biostat(struct bio *bio)
 {
@@ -89,8 +111,27 @@ static void free_biostat(struct biostat *bs)
     kfree(bs);
 }
 
+/* must be called with blkstat.infolock held */
+void update_info(int rw, unsigned long rtime)
+{
+    // FIXME:%% int rtime_int = rtime;
+
+    blkstat.info.ios[rw]++;
+    blkstat.info.duration[rw] += rtime;
+
+    if (blkstat.info.maxrt < rtime)
+        blkstat.info.maxrt = rtime;
+    if (blkstat.info.minrt > rtime)
+        blkstat.info.minrt = rtime;
+    atomic_dec(&blkstat.qdepth);
+
+    /* TODO:%% Update fifo */
+}
+
 static void blkstat_endio(struct bio *cloned_bio, int error)
 {
+    unsigned long flags;
+
     struct biostat *bs = cloned_bio->bi_private;
     struct bio *bio = bs->bio;
    
@@ -101,6 +142,14 @@ static void blkstat_endio(struct bio *cloned_bio, int error)
 
     pr_info("%s: endio -- elapsed: %lu -- size: %u -- up-to-date: %d-- error: %d -- IRQ: %lu -- INTR: %lu\n", 
         DEVNAME, nselapsed, cloned_bio->bi_iter.bi_size, uptodate, error, in_irq(), in_interrupt());
+
+    /*
+     * It seems that we may get called both from a non-IRQ and IRQ context,
+     * so use the irq-saving version, to be on the safe side.
+     */
+    spin_lock_irqsave(&blkstat.infolock, flags);
+    update_info(bio_data_dir(cloned_bio) & REQ_WRITE, nselapsed);
+    spin_unlock_irqrestore(&blkstat.infolock, flags);
 
     bio_endio(bio, error);
     free_biostat(bs);    
@@ -143,6 +192,7 @@ static void blkstat_make_request(struct request_queue *q, struct bio *bio)
     cloned_bio->bi_bdev = blkstat.tdev;
     cloned_bio->bi_end_io = blkstat_endio;
 
+    atomic_inc(&blkstat.qdepth);
     generic_make_request(cloned_bio);
     return;
 
@@ -223,20 +273,98 @@ int blkstat_getgeo(struct block_device * block_device, struct hd_geometry * geo)
 	return 0;
 }
 
-/*
- * The device operations structure.
- */
 static struct block_device_operations blkstat_ops = {
     .owner  = THIS_MODULE,
 	.getgeo = blkstat_getgeo,
 };
 
+/* FIXME: mutex - concurrent userspace */
+static void *blkstat_seq_start(struct seq_file *sf, loff_t *pos)
+//    __acquires(devinfo.list_lock)
+{
+    unsigned long flags;
+
+    /* grab the info spinlock and take a snapshot of current statistics */
+    spin_lock_irqsave(&blkstat.infolock, flags);
+    memcpy(&userinfo, &blkstat.info, sizeof(userinfo));
+    userinfo.qdepth = atomic_read(&blkstat.qdepth);
+    userinfo.rtcnt = 0;     // FIXME: fifo = ififo_len();
+    spin_unlock_irqrestore(&blkstat.infolock, flags);
+
+    return *pos == 1 ? SEQ_START_TOKEN : NULL;
+}
+
+static void *blkstat_seq_next(struct seq_file *sf, void *v, loff_t *pos)
+{
+    return NULL;
+#if 0
+    (*pos)++;
+    return (void *) (long) pos;
+#endif
+}
+
+/* FIXME: mutex - concurrent userspace */
+static void blkstat_seq_stop(struct seq_file *sf, void *v)
+//    __releases(devinfo.list_lock)
+{
+}
+
+static int blkstat_seq_show(struct seq_file *sf, void *v)
+{
+    struct binfo *info = &userinfo.info;
+    //%% long pos;
+
+    /* the first item => header */
+    if (v == SEQ_START_TOKEN) {
+        long meanrt = (info->duration[0] + info->duration[1]) / 
+                (info->ios[0] + info->ios[1]);
+        seq_printf(sf, "Target device: %s\n", targetname);
+        seq_printf(sf, "Read I/Os: %lu -- I/Os per sec: %lu", info->ios[0], info->duration[0]/info->ios[0]);
+        seq_printf(sf, "Write I/Os: %lu -- I/Os per sec: %lu", info->ios[1], info->duration[1]/info->ios[1]);
+        seq_printf(sf, "Queue depth: %d\n", userinfo.qdepth);
+        seq_printf(sf, "I/O service time (ns)\n");
+        seq_printf(sf, "Min: %lu -- Max: %lu\n", info->minrt, info->maxrt);
+        seq_printf(sf, "Mean: %lu\n", meanrt);
+        if (userinfo.rtcnt >= MIN_SAMPLES) {
+            // FIXME: fifo
+        }
+        return 0;
+    }
+
+    //%% seq_printf(sf, "Time: %10i.%06i\n", (int) time->tv.tv_sec, (int) time->tv.tv_usec);
+    return 0;
+}
+
+static struct seq_operations seq_ops = {
+    .start  = blkstat_seq_start,
+    .next   = blkstat_seq_next,
+    .stop   = blkstat_seq_stop,
+    .show   = blkstat_seq_show,
+};
+
+static int blkstat_seq_open(struct inode *inode, struct file *file)
+{
+    return seq_open(file, &seq_ops);
+}
+
+static struct file_operations proc_fops = {
+    .open       = blkstat_seq_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = seq_release,
+};
+
+static struct proc_dir_entry *proc_entry;
+
 static int __init blkstat_init(void)
 {
+    int rc;
+
     if (strcmp(targetname, "") == 0)
         return -EINVAL;
 
-	spin_lock_init(&blkstat.lock);
+    if (nrsamples < MIN_SAMPLES)
+        return -EINVAL;
 
 	/* 
      * Use blk_alloc_queue() to set up the 'bio-oriented' processing,
@@ -246,10 +374,9 @@ static int __init blkstat_init(void)
      * and register a callback function that deals with individual
      * requests. FIXME: check on request-oriented.
      */
-    if (!(blkstat.queue = blk_alloc_queue(GFP_KERNEL)))
-    {
-        pr_info("blkstat: alloc_queue failed\n");
-        return -EFAULT;
+    if (!(blkstat.queue = blk_alloc_queue(GFP_KERNEL))) {
+        pr_info("%s: alloc_queue failed\n", DEVNAME);
+        return -ENXIO;
     }
 
     /* register our make_request() callback */
@@ -258,15 +385,14 @@ static int __init blkstat_init(void)
 	blk_queue_logical_block_size(blkstat.queue, LOGICAL_BLOCK_SIZE);
 
 	/* Register the driver for our logical device */
-	if ((majornr = register_blkdev(majornr, DEVNAME)) < 0)
-    {
-		pr_info("blkstat: unable to get major number\n");
-		goto error_after_alloc_queue;
+	if ((majornr = register_blkdev(majornr, DEVNAME)) < 0) {
+		pr_info("%s: unable to get major number\n", DEVNAME);
+		goto error_rm_queue;
 	}
 
 	/* Populate the gendisk structure */
 	if (!(blkstat.gendisk = alloc_disk(NR_MINORS)))
-		goto error_after_redister_blkdev;
+		goto error_rm_dev;
 
 	blkstat.gendisk->major = majornr;
 	blkstat.gendisk->first_minor = 0;
@@ -276,20 +402,25 @@ static int __init blkstat_init(void)
 	blkstat.gendisk->queue = blkstat.queue;
 	add_disk(blkstat.gendisk);
 
-    pr_info("blkstat: disk init done\n");
+    pr_info("%s: disk init done\n", DEVNAME);
 
-	return blkstat_start(targetname);
+	rc = blkstat_start(targetname);
 
-error_after_redister_blkdev:
+    proc_entry = proc_create(PROC_ENTRY, 0666, NULL, &proc_fops);
+    return rc;
+
+error_rm_dev:
 	unregister_blkdev(majornr, DEVNAME);
 
-error_after_alloc_queue:
+error_rm_queue:
     blk_cleanup_queue(blkstat.queue);
-	return -EFAULT;
+	return -ENXIO;
 }
 
 static void __exit blkstat_exit(void)
 {
+    remove_proc_entry(PROC_ENTRY, NULL);
+
     if (blkstat.is_active) {
         blkdev_put(blkstat.tdev, STACKBD_BDEV_MODE);
         bdput(blkstat.tdev);
